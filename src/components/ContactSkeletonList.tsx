8b2e5ea6ba66d97e20b29b2a95701387ec108421
Dmitriy Solovev <dmptts@gmail.com>
2023-01-26 12:29:17 +0800

    Исправляет ошибку с отрисовкой склета списка контактов

--- src/components/ContactSkeletonList.tsx
+++ src/components/ContactSkeletonList.tsx
@@ -1,12 +1,12 @@
 import ContactItemSkeleton from './ContactItemSkeleton';
 
 export default function ContactSkeletonList() {
-  const arr = new Array(10);
+  const arr = new Array(10).fill(<ContactItemSkeleton />);
   return (
     <>
-      {arr.map(() => (
-        <ContactItemSkeleton />
-      ))}
+      {arr.map((_, i) => {
+        return <ContactItemSkeleton key={i} />;
+      })}
     </>
   );
 }
