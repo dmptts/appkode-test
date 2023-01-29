273e7b64db4465c33a9b443dc9c3a2d49bdf0400
Dmitriy Solovev <dmptts@gmail.com>
2023-01-29 18:40:56 +0800

    Удаляет неиспользуемый файл

--- src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-export default function Error() {
-  return (
-    <div>
-      <b>Какой-то сверхразум все сломал</b>
-      <p>Постараемся быстро починить</p>
-      <button type="button">Попрообовать снова</button>
-    </div>
-  );
-}
